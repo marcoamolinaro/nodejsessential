bd43da8389ec2981d9459a5cbdda701f95ebc2a1
Marco Aurelio Molinaro <marcomolinario.provider@azulseguros.com.br>
2020-08-31 16:39:21 -0300

    Core modules

---
+++ core.js
@@ -0,0 +1,14 @@
+const path = require("path");
+//const util = require("util");
+//const v8 = require("v8");
+const {log} = require("util");
+const {getHeapCodeStatistics} = require("v8");
+//const dirUploads = path.join(__dirname, "www", "files", "uploads");
+
+//console.log(dirUploads);
+
+//util.log(path.basename(__filename));
+
+//util.log(" ^ The name of the current file");
+
+log(getHeapCodeStatistics());
